7b81e8359f5901db8696cd460c911705a8fe26c4
Eran Kaufman <erank3@gmail.com>
2015-03-18 01:05:13 -0700

    enable cors support

--- gliist_server/App_Start/WebApiConfig.cs
+++ gliist_server/App_Start/WebApiConfig.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 using System.Net.Http.Headers;
+using System.Web.Http.Cors;
 
 namespace gliist_server
 {
@@ -19,6 +20,8 @@ namespace gliist_server
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
