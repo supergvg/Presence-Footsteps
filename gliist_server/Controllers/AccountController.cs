a3a1e885abb82bb7fb0643feb54be24ac4d67113
Eran Kaufman <erank3@gmail.com>
2015-03-17 23:27:55 -0700

    test

--- gliist_server/Controllers/AccountController.cs
+++ gliist_server/Controllers/AccountController.cs
@@ -34,7 +34,7 @@ namespace gliist_server.Controllers
             ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
         {
             UserManager = userManager;
-            AccessTokenFormat = accessTokenFormat;//test
+            AccessTokenFormat = accessTokenFormat;
         }
 
         public UserManager<IdentityUser> UserManager { get; private set; }
