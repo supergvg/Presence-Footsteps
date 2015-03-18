bae6e9f93070bad35f026624d80a769fca979a1d
Eran Kaufman <erank3@gmail.com>
2015-03-17 23:31:13 -0700

    add event file

---
+++ gliist_server/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace gliist_server.Models
+{
+    public class EventDBContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+    }
+
+    public class Event
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime date { get; set; }
+
+    }
+}
