95b143aba9b2a163096a7307fab437cd5cf8c5c3
Eran Kaufman <erank3@gmail.com>
2015-03-26 01:45:31 -0700

    cors

--- gliist_server/Controllers/EventController.cs
+++ gliist_server/Controllers/EventController.cs
@@ -16,6 +16,7 @@ using System.Web.Http.Cors;
 
 namespace gliist_server.Controllers
 {
+    [Authorize]
     [EnableCors("*", "*", "*")]
     public class EventController : ApiController
     {
--- gliist_server/Global.asax.cs
+++ gliist_server/Global.asax.cs
@@ -19,5 +19,16 @@ namespace gliist_server
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (Context.Request.Path.Contains("api/") && Context.Request.HttpMethod == "OPTIONS")
+            {
+                Context.Response.AddHeader("Access-Control-Allow-Origin", "*");
+                Context.Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                Context.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST PUT, DELETE, OPTIONS");
+                Context.Response.End();
+            }
+        } 
     }
 }
