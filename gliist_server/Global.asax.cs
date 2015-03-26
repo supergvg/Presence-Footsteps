bec014575a1493cb301ff9204d5fb77d6da093a1
Eran Kaufman <erank3@gmail.com>
2015-03-26 01:47:01 -0700

    cors!!!!

--- gliist_server/Global.asax.cs
+++ gliist_server/Global.asax.cs
@@ -25,10 +25,10 @@ namespace gliist_server
             if (Context.Request.Path.Contains("api/") && Context.Request.HttpMethod == "OPTIONS")
             {
                 Context.Response.AddHeader("Access-Control-Allow-Origin", "*");
-                Context.Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                Context.Response.AddHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
                 Context.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST PUT, DELETE, OPTIONS");
                 Context.Response.End();
             }
-        } 
+        }
     }
 }
