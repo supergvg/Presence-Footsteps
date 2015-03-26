d1482e27bad7388403d004ddc84ae6f2442f2994
Eran Kaufman <erank3@gmail.com>
2015-03-26 00:58:37 -0700

    cors!!!@#@$

--- gliist_server/Startup.cs
+++ gliist_server/Startup.cs
@@ -12,6 +12,8 @@ namespace gliist_server
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
             ConfigureAuth(app);
         }
     }
