541563c00a34c65f6d688569b996b1b5b3c1c263
Eran Kaufman <erank3@gmail.com>
2015-03-27 00:01:46 -0700

    cors!

--- gliist_server/App_Start/WebApiConfig.cs
+++ gliist_server/App_Start/WebApiConfig.cs
@@ -14,7 +14,7 @@ namespace gliist_server
     {
         public static void Register(HttpConfiguration config)
         {
-            //config.EnableCors();
+            config.EnableCors();
 
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
