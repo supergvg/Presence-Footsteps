6db41eaf5ff128ba856b22b3a27cdee8f7c6c866
Eran Kaufman <erank3@gmail.com>
2015-05-26 14:01:35 -0700

    alerts

--- app/www/app/alerts/controllers/alerts-controller.js
+++ app/www/app/alerts/controllers/alerts-controller.js
@@ -5,21 +5,32 @@ angular.module('starter').controller('alertsController', ['$scope', '$rootScope'
         $scope.currentUser = $rootScope.currentUser;
 
 
-        $scope.alerts = [
-            {
-                message: 'alert1',
-                time: '1 minute'
-            },
-            {
-                message: 'alert2',
-                time: '1 minute'
-            },
-            {
-                message: 'alert3',
-                time: '1 minute'
-            }
-        ];
+        $scope.doAlertsRefresh = function () {
+            alert('refresh!');
+        };
+        $scope.init = function () {
 
+
+            //eventsService.getAlerts().then().finally();
+
+            /*$scope.alerts = [
+             {
+             message: 'alert1',
+             time: '1 minute'
+             },
+             {
+             message: 'alert2',
+             time: '1 minute'
+             },
+             {
+             message: 'alert3',
+             time: '1 minute'
+             }
+             ];*/
+
+            $scope.alerts = [];
+
+        };
 
         $rootScope.$on('$cordovaPush:notificationReceived', function (event, notification) {
 
