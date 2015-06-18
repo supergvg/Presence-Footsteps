fe54a17fc7423d715051ec8602427a22820c947e
Eran Kaufman <erank3@gmail.com>
2015-06-17 20:26:20 -0700

    final fixes

--- webapp/src/app/user/controllers/account.controller.js
+++ webapp/src/app/user/controllers/account.controller.js
@@ -1,39 +1,66 @@
 'use strict';
 
 angular.module('gliist')
-    .controller('AccountDetailsCtrl', ['$scope', '$mdDialog', 'userService', 'dialogService', '$state',
-        function ($scope, $mdDialog, userService, dialogService, $state) {
+  .controller('AccountDetailsCtrl', ['$scope', '$mdDialog', 'userService', 'dialogService', '$state',
+    function ($scope, $mdDialog, userService, dialogService, $state) {
 
 
-            $scope.linkNewAccount = function (ev) {
-                var scope = $scope.$new();
+      $scope.linkNewAccount = function (ev) {
+        var scope = $scope.$new();
 
-                $mdDialog.show({
-                    controller: 'InviteUserCtrl',
-                    scope: scope,
-                    templateUrl: 'app/user/templates/link-account-dialog.html',
-                    targetEvent: ev
-                });
-            };
+        $mdDialog.show({
+          controller: 'InviteUserCtrl',
+          scope: scope,
+          templateUrl: 'app/user/templates/link-account-dialog.html',
+          targetEvent: ev
+        });
+      };
 
-            $scope.editUser = function () {
-                alert('edit user!');
-            };
+      $scope.editUser = function (ev, linked_account) {
+        var scope = $scope.$new();
+        scope.linked_account = linked_account;
 
-            $scope.init = function () {
-                $scope.initializing = true;
-                userService.getCompanyInfo().then(
-                    function (data) {
-                        $scope.company = data;
-                    },
-                    function () {
-                        dialogService.error('Oops there was a problem loading account info, please try again')
-                    }
-                ).finally(
-                    function () {
-                        $scope.initializing = false;
-                    }
-                );
-            };
+        $mdDialog.show({
+          controller: 'InviteUserCtrl',
+          scope: scope,
+          templateUrl: 'app/user/templates/link-account-dialog.html',
+          targetEvent: ev
+        });
 
-        }]);
+      };
+
+
+      $scope.refresh = function () {
+
+        $scope.refreshing = true;
+        userService.getCompanyInfo().then(
+          function (data) {
+            $scope.company = data;
+          },
+          function () {
+            dialogService.error('Oops there was a problem loading account info, please try again')
+          }
+        ).finally(
+          function () {
+            $scope.refreshing = false;
+          }
+        );
+      };
+
+      $scope.init = function () {
+        $scope.initializing = true;
+        userService.getCompanyInfo().then(
+          function (data) {
+            $scope.company = data;
+          },
+          function () {
+            dialogService.error('Oops there was a problem loading account info, please try again')
+          }
+        ).finally(
+          function () {
+            $scope.initializing = false;
+          }
+        );
+      };
+
+    }]);
