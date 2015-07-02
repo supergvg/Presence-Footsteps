d92f871a54af0c673c95bada3e09f4d0d33f4158
Eran Kaufman <erank3@gmail.com>
2015-07-02 12:47:07 +0300

    add android version

--- webapp/src/app/user/controllers/signup.controller.js
+++ webapp/src/app/user/controllers/signup.controller.js
@@ -1,51 +1,51 @@
 'use strict';
 
 angular.module('gliist')
-    .controller('SignupCtrl', ['$scope', '$mdDialog', 'userService', 'dialogService', '$state',
-        function ($scope, $mdDialog, userService, dialogService, $state) {
+  .controller('SignupCtrl', ['$scope', '$mdDialog', 'userService', 'dialogService', '$state',
+    function ($scope, $mdDialog, userService, dialogService, $state) {
 
 
-            $scope.displayErrorMessage = function (field) {
-                return ($scope.showValidation) || (field.$touched && field.$error.required);
-            };
+      $scope.displayErrorMessage = function (field) {
+        return ($scope.showValidation) || (field.$touched && field.$error.required);
+      };
 
 
-            $scope.hide = function () {
-                $scope.user.username = null;
-                $scope.user.password = null;
-                $scope.user.confirmPassword = null;
-                $mdDialog.hide();
-            };
-            $scope.cancel = function () {
-                $mdDialog.cancel();
-            };
+      $scope.hide = function () {
+        $scope.user.username = null;
+        $scope.user.password = null;
+        $scope.user.confirmPassword = null;
+        $mdDialog.hide();
+      };
+      $scope.cancel = function () {
+        $mdDialog.cancel();
+      };
 
-            $scope.register = function (form) {
+      $scope.register = function (form) {
 
-                if (form && form.$invalid) {
-                    $scope.showValidation = true;
-                    return;
-                }
+        if (form && form.$invalid) {
+          $scope.showValidation = true;
+          return;
+        }
 
-                if ($scope.user.password !== $scope.user.confirmPassword) {
-                    return;
-                }
+        if ($scope.user.password !== $scope.user.confirmPassword) {
+          return;
+        }
 
 
-                var inviteMode = $scope.options ? $scope.options.inviteMode : null;
-                $scope.fetchingData = true;
-                userService.registerEmail($scope.user, inviteMode).then(function () {
-                    $scope.hide();
-                    $state.go('main');
-                }, function (err) {
-                    dialogService.error(JSON.stringify(err));
-                    dialogService.error('There was a problem signing up, please try again');
-                }).
-                    finally(
-                    function () {
-                        $scope.fetchingData = false;
-                    }
-                );
-            };
+        var inviteMode = $scope.options ? $scope.options.inviteMode : null;
+        $scope.fetchingData = true;
+        userService.registerEmail($scope.user, inviteMode).then(function () {
+          $scope.hide();
+          $state.go('main.welcome');
+        }, function (err) {
+          dialogService.error(JSON.stringify(err));
+          dialogService.error('There was a problem signing up, please try again');
+        }).
+          finally(
+          function () {
+            $scope.fetchingData = false;
+          }
+        );
+      };
 
-        }]);
+    }]);
