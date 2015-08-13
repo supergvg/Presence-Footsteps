4b50a338719fc2df4c3c122babc0d7df34710cef
Eran Kaufman <erank3@gmail.com>
2015-08-13 13:51:26 -0700

    app fixes

--- webapp/src/app/directives/ignore-time-zone.js
+++ webapp/src/app/directives/ignore-time-zone.js
@@ -4,6 +4,12 @@ angular.module('gliist').
             if (!val) {
                 return;
             }
+
+            var a = val.split(/[^0-9]/);
+
+            var d = new Date(a[0], a[1] - 1, a[2], a[3], a[4], a[5]);
+            return d;
+
             var newDate = new Date(val.replace('T', ' ').slice(0, -6));
             return newDate;
         };
