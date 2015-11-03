4bcbce37878c68be323893d82e864b2cd2604004
Eran Kaufman <erank3@gmail.com>
2015-11-03 01:00:59 -0800

    add multiple guests support

--- app/www/app/guests/controllers/add-guest-controller.js
+++ app/www/app/guests/controllers/add-guest-controller.js
@@ -1,16 +1,64 @@
 angular.module('starter').controller('addGuestController', ['$scope', '$stateParams', 'eventsService', 'dialogService', '$state', '$rootScope', '$ionicLoading',
 
     function ($scope, $stateParams, eventsService, dialogService, $state, $rootScope, $ionicLoading) {
-        $scope.currentGuest = {};
 
+        $scope.currentGuest = {};
+        $scope.guests = {};
+
+        $scope.active = 'single';
+        $scope.setActive = function (activeScreen) {
+            $scope.active = activeScreen;
+            $scope.addMore = false;
+            $scope.errorMessage = '';
+        };
+
+        $scope.isActive = function (activeScreen) {
+            if ($scope.active === activeScreen) {
+                return {
+                    'background-color': 'white',
+                    'color': '#9FCBFF'
+                }
+            }
+        };
 
         $scope.onAddMoreClicked = function () {
             $scope.currentGuest = {};
+            $scope.guests = {};
             $scope.addMore = false;
         };
 
         $scope.formInvalid = function () {
             return (!$scope.currentGuest.firstName || !$scope.currentGuest.lastName);
+        };
+
+
+        $scope.onAddMultipleClicked = function () {
+
+            if (!$scope.guests.names) {
+                $scope.errorMessage = "Please enter guests names separated with commas";
+                return;
+            }
+
+            $scope.errorMessage = null;
+            $scope.fecthingData = true;
+
+            eventsService.addGuestsToEvent($scope.guests, $scope.currentEvent.id).then(
+                function () {
+                    $scope.addMore = true;
+                    dialogService.success('Guests added');
+                },
+                function () {
+                    dialogService.error('Oops there was a problem adding guest, please try again');
+                }
+            ).finally(function () {
+                    $scope.fecthingData = false;
+                });
+        };
+
+        $scope.onAddMoreMultipleClicked = function () {
+            $scope.currentGuest = {};
+            $scope.guests = {};
+            $scope.addMore = false;
         };
 
         $scope.onAddClicked = function () {
