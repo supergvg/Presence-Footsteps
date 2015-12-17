4847f00beedf71615a6d17cf2f70c038ceaac995
ychuyan <ychuyan@ideas-implemented.com>
2015-12-17 10:18:57 +0300

    update migrations

--- gliist_server/Migrations/Configuration.cs
+++ gliist_server/Migrations/Configuration.cs
@@ -9,8 +9,8 @@ namespace gliist_server.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
+            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationDataLossAllowed = false;
         }
 
         protected override void Seed(gliist_server.Models.EventDBContext context)
