87dcf861fc09f7a8107d01a48cf1a27d48b297a9
Drupal <pkahanov@ideas-implemented.com>
2016-01-21 17:16:28 +0300

    some changes

--- webapp/src/app/index.js
+++ webapp/src/app/index.js
@@ -47,7 +47,7 @@ angular.module('gliist', [
                 .backgroundPalette('grey');
 
             window.redirectUrl = 'http://gjests.azurewebsites.net/';
-	    window.redirectUrl = 'http://gjests-api.ideas-implemented.com/';
+//	    window.redirectUrl = 'http://gjests-api.ideas-implemented.com/';
             $provide.factory('myHttpInterceptor', function () {
                 return {
                     'request': function (config) {
