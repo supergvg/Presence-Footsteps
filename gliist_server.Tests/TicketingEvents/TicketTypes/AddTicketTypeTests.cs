6d7e5569da8cc76dac2ba79cd06ccb504bef43de
opestrikov <opestrikov@ideas-implemented.com>
2016-01-22 13:14:14 +0300

    working on unit testing ticket types

--- gliist_server.Tests/TicketingEvents/TicketTypes/AddTicketTypeTests.cs
+++ gliist_server.Tests/TicketingEvents/TicketTypes/AddTicketTypeTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
 using System.Web.Http.Results;
 using gliist_server.Areas.Ticketing.Controllers;
 using gliist_server.Areas.Ticketing.Models;
