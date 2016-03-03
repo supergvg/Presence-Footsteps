f459ec91ca1ab89225d3d45586e074ff6929d22b
Roman Makarov <roman.makarov@gmail.com>
2016-03-03 11:01:05 +0300

    Notes field added

--- gliist_server/Controllers/GuestController.cs
+++ gliist_server/Controllers/GuestController.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
@@ -10,11 +11,9 @@ using gliist_server.Models;
 
 namespace gliist_server.Controllers
 {
-
     [Authorize]
     public class GuestController : ApiController
     {
-
         private EventDBContext db = new EventDBContext();
         private UserManager<UserModel> UserManager;
 
@@ -95,6 +94,22 @@ namespace gliist_server.Controllers
             return CreatedAtRoute("DefaultApi", new { id = guest.id }, guest);
         }
 
+        // POST api/Guest
+        [ResponseType(typeof(Guest))]
+        public IHttpActionResult UpdateNotes(GuestNotesModel guestNotesModel)
+        {
+            if (guestNotesModel == null)
+                throw new ArgumentNullException("guestNotesModel");
+
+            var guest = db.Guests.FirstOrDefault(x => x.id == guestNotesModel.GuestId);
+            if (guest == null)
+                return BadRequest("Guest not found");
+
+            guest.phoneNumber = guestNotesModel.Notes ?? string.Empty;
+            db.SaveChanges();
+            return Ok();
+        }
+
         // DELETE api/Guest/5
         [ResponseType(typeof(Guest))]
         public async Task<IHttpActionResult> DeleteGuest(int id)
@@ -124,7 +139,6 @@ namespace gliist_server.Controllers
         {
             return db.Guests.Count(e => e.id == id) > 0;
         }
-
 
         public GuestController()
         {
