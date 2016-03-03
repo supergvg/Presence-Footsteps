af033e9f8d13313c9399b9a87549ce8b67dda881
Drupal <pkahanov@ideas-implemented.com>
2016-03-03 18:41:17 +0300

    rsvp visitors stat and export

--- webapp/src/app/events/controllers/events.stats.controller.js
+++ webapp/src/app/events/controllers/events.stats.controller.js
@@ -20,6 +20,7 @@ angular.module('gliist')
                 {name: 'RSVP', color: '#f7f9c3', total: 0},
                 {name: 'RSVP checked in', color: '#ffd8b4', total: 0}
             ];
+            $scope.rsvpTotalVisitors = 0;
             $scope.Math = $window.Math;
             $scope.gliOptions = {
                 readOnly: true,
@@ -50,20 +51,34 @@ angular.module('gliist')
                     $scope.stats[category].total += gl.guestsCount;
                     $scope.totalCheckedIn += $scope.stats[category].totalCheckedIn;
                     
-                    //RSVP stats
-                    if (gl.instanceType === 2 && gl.published) {
-                        $scope.rsvp[0].total += gl.guestsCount;
-                        angular.forEach(gl.actual, function(guest) {
-                            $scope.rsvp[1].total += guest.plus + 1;
-                        });
-                        $scope.rsvp[2].total = $scope.stats[category].totalCheckedIn;
-                    }
-                    if (gl.instanceType === 4) {
-                        $scope.rsvp[1].total += gl.guestsCount;
-                        $scope.rsvp[2].total = $scope.stats[category].totalCheckedIn;
+                    if ($scope.isRSVP()) {
+                        //RSVP stats
+                        if (gl.instanceType === 2 && gl.published) {
+                            $scope.rsvp[0].total += gl.guestsCount;
+                            angular.forEach(gl.actual, function(guest) {
+                                $scope.rsvp[1].total += guest.plus + 1;
+                            });
+                            $scope.rsvp[2].total = $scope.stats[category].totalCheckedIn;
+                        }
+                        if (gl.instanceType === 4) {
+                            $scope.rsvp[1].total += gl.guestsCount;
+                            $scope.rsvp[2].total = $scope.stats[category].totalCheckedIn;
+                        }
                     }
                 });
+                eventsService.getRSVPVisitors($scope.event.id).then(
+                    function(data) {
+                    }
+                );
                 $scope.updateChart();
+            };
+            
+            $scope.isRSVP = function() {
+                return $scope.eventType === 2;
+            };
+            
+            $scope.getExportExcelUrl = function() {
+                return $window.redirectUrl+'api/reports/exportrsvp/'+$scope.event.id;
             };
             
             $scope.getCategoryStats = function(category) {
@@ -96,38 +111,40 @@ angular.module('gliist')
                     });
                 });
                 
-                $scope.rsvp.sort(function(a, b){
-                    if (a.total > b.total) {
-                        return 1;
-                    }
-                    if (a.total < b.total) {
-                        return -1;
-                    }
-                });
-                var names = [''],
-                    totals = [''];
-                $scope.rsvp.forEach(function(item, i, arr){
-                    names.push(item.name);
-                    names.push({role: 'tooltip'});
-                    if (i > 0) {
-                        totals.push(arr[i].total - arr[i - 1].total);
-                    } else {
-                        totals.push(item.total);
-                    }
-                    totals.push(item.name+': '+item.total);
-                });
-                $scope.chartObjectRSVP = {
-                    type: 'ColumnChart',
-                    options: {
-                        legend: 'none',
-                        colors: $scope.rsvp.map(function(item){ return item.color; }),
-                        chartArea: {left: '5%', top: '5%', width: '100%', height: '90%'},
-                        bar: { groupWidth: '20%' },
-                        isStacked: true,
-                        vAxis: {gridlines: {count: 3}, viewWindow: {max: $scope.rsvp[$scope.rsvp.length - 1].total}}
-                    },
-                    data: [names, totals]
-                };
+                if ($scope.isRSVP()) {
+                    $scope.rsvp.sort(function(a, b){
+                        if (a.total > b.total) {
+                            return 1;
+                        }
+                        if (a.total < b.total) {
+                            return -1;
+                        }
+                    });
+                    var names = [''],
+                        totals = [''];
+                    $scope.rsvp.forEach(function(item, i, arr){
+                        names.push(item.name);
+                        names.push({role: 'tooltip'});
+                        if (i > 0) {
+                            totals.push(arr[i].total - arr[i - 1].total);
+                        } else {
+                            totals.push(item.total);
+                        }
+                        totals.push(item.name+': '+item.total);
+                    });
+                    $scope.chartObjectRSVP = {
+                        type: 'ColumnChart',
+                        options: {
+                            legend: 'none',
+                            colors: $scope.rsvp.map(function(item){ return item.color; }),
+                            chartArea: {left: '5%', top: '5%', width: '100%', height: '90%'},
+                            bar: { groupWidth: '20%' },
+                            isStacked: true,
+                            vAxis: {gridlines: {count: 3}, viewWindow: {max: $scope.rsvp[$scope.rsvp.length - 1].total}}
+                        },
+                        data: [names, totals]
+                    };
+                }
             };
 
             $scope.init = function() {
