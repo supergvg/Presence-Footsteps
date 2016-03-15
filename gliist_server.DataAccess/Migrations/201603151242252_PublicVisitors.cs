74d639ceac69ff61df20459d8c7996c8cedb0942
opestrikov <opestrikov@ideas-implemented.com>
2016-03-15 15:50:35 +0300

    fix PublicVisitors migration

--- gliist_server.DataAccess/Migrations/201603151242252_PublicVisitors.cs
+++ gliist_server.DataAccess/Migrations/201603151242252_PublicVisitors.cs
@@ -1,13 +1,12 @@
-namespace gliist_server.Migrations
+using System.Data.Entity.Migrations;
+
+namespace gliist_server.DataAccess.Migrations
 {
-    using System;
-    using System.Data.Entity.Migrations;
-    
     public partial class PublicVisitors : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.Events", "PublicVisitors", c => c.Int(nullable: false, defaultValue: 0));
+            AddColumn("dbo.Events", "PublicVisitors", c => c.Int(false, defaultValue:0));
         }
         
         public override void Down()
