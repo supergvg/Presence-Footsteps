60efd3dace6bca8d3ef19800474afa74fa40b246
Drupal <pkahanov@ideas-implemented.com>
2016-04-13 10:38:36 +0300

    stripe

--- webapp/src/app/user/controllers/billing.controller.js
+++ webapp/src/app/user/controllers/billing.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('gliist')
-    .controller('BillingCtrl', ['$scope', '$mdDialog', 'subscriptionsService',
-        function($scope, $mdDialog, subscriptionsService) {
+    .controller('BillingCtrl', ['$scope', '$mdDialog', 'subscriptionsService', 'stripe',
+        function($scope, $mdDialog, subscriptionsService, stripe) {
             $scope.loading = true;
             $scope.invoices = [
                 {
@@ -47,5 +47,19 @@ angular.module('gliist')
                     scope: scope,
                     templateUrl: 'app/user/templates/invoice-details.html'
                 });  
+            };
+
+            $scope.selectPlan = function(index) {
+                var selectedPlan = $scope.plans[index];
+                if ($scope.combinedPlanWith[selectedPlan.keyForUI] && $scope.combinedPlanWith[selectedPlan.keyForUI].model !== selectedPlan.keyForUI) {
+                    selectedPlan = $scope.combinedPlanWith[selectedPlan.keyForUI].options[0];
+                }
+                if (selectedPlan.paymentType === 'select') {
+                    if (selectedPlan.amount > 0) {
+                        stripe.openCheckout({description: selectedPlan.name + ' plan', amount: selectedPlan.amount});
+                    } else {
+                        
+                    }
+                }
             };
         }]);
