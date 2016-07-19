4506d2bc3664ffcf1c4db3194ae54fe948b9a8ca
GVG <gvgvit@gmail.com>
2016-07-19 16:25:39 +0300

    #33233 Error message. New password requirements

--- src/app/user/controllers/signup.controller.js
+++ src/app/user/controllers/signup.controller.js
@@ -28,7 +28,11 @@ angular.module('gliist')
                     $scope.user.confirmPassword = null;
                     $state.go('main.welcome');
                 }, function(error) {
-                    var message = error && error.Message || 'There was a problem signing up, please try again';
+                    var message = 'There was a problem signing up, please try again';
+                    if (error.ModelState)
+                        for (var err in error.ModelState)
+                        message = error.ModelState[err][0] + '\n';
+
                     dialogService.error(message);
                 }).finally(function() {
                     $scope.sendingData = false;
@@ -50,8 +54,8 @@ angular.module('gliist')
                         $scope.user.username = userInfo.email;
                         $scope.user.company = company;
                         $scope.user.token = token;
-                    }, function() {
-                        dialogService.error('There was a problem signing up, please try again');
+                    }, function(error) {
+                        dialogService.error(error && error.Message || 'There was a problem signing up, please try again');
                     }).finally(function() {
                         $scope.loading = false;
                     });
