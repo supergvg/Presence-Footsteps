3d9aa3173e49c85914e77cad2a73783a7886799d
GVG <gvgvit@gmail.com>
2016-07-21 03:25:04 +0300

    #33460 null details fix

--- src/app/filters/nl2br.filter.js
+++ src/app/filters/nl2br.filter.js
@@ -4,6 +4,8 @@ angular.module('gliist')
     .filter('nl2br', ['$sanitize', function($sanitize) {
     var tag = (/xhtml/i).test(document.doctype) ? '<br />' : '<br>';
     return function(msg) {
+        if (!msg)
+            return '';
         // ngSanitize's linky filter changes \r and \n to &#10; and &#13; respectively
         msg = (msg + '').replace(/(\r\n|\n\r|\r|\n|&#10;&#13;|&#13;&#10;|&#10;|&#13;)/g, tag + '$1');
         return $sanitize(msg);
