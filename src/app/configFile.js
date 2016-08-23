8146e1a1db10d6a3a8ba13ee669c156f76f3bbf6
Drupal <pkahanov@ideas-implemented.com>
2016-08-23 11:38:43 +0300

    refs task GJES-18

--- src/app/configFile.js
+++ src/app/configFile.js
@@ -1,2 +1,2 @@
 angular.module("gliist")
-.constant("EnvironmentConfig", {"gjests_api":"http://staging-app.gjests.com/","subscriptions_api":"http://staging-subscriptions.gjests.com/api/","payments_api":"http://staging-payments.gjests.com/api/"});
+.constant("EnvironmentConfig", {"gjests_api":"http://gjests-api.ideas-implemented.com/","subscriptions_api":"http://54.228.219.224:8005/api/","payments_api":"http://54.228.219.224:8006/api/"});
