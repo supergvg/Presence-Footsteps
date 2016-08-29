333d321cc64944abeb2de9d4a13623d18595c0f5
GVG <gvgvit@gmail.com>
2016-08-29 22:35:37 +0300

    GJES-73

--- src/app/events/controllers/events.stats.controller.js
+++ src/app/events/controllers/events.stats.controller.js
@@ -59,7 +59,7 @@ angular.module('gliist')
                             $scope.totalCheckedIn += guest.guest.plus + 1 - guest.plus;
                         }
                     });
-                    if (gl.instanceType === 2 && gl.instanceType === 4) //include all except for unpublished private RSVP
+                    if (gl.instanceType !== 2 && gl.instanceType !== 4) //include all except for unpublished private RSVP
                         $scope.stats[category].total += gl.guestsCount;                
                     
                     if ($scope.isRSVP()) {
@@ -70,12 +70,13 @@ angular.module('gliist')
                                 $scope.rsvp[1].total += guest.guest.plus + 1;
                             });
                             $scope.rsvp[2].total = $scope.stats[category].totalCheckedIn;
+                            $scope.stats[category].total = $scope.rsvp[1].total;
                         }
                         if (gl.instanceType === 4) {
                             $scope.rsvp[1].total += gl.guestsCount;
                             $scope.rsvp[2].total = $scope.stats[category].totalCheckedIn;
+                            $scope.stats[category].total = $scope.rsvp[1].total;
                         }
-                        $scope.stats[category].total = $scope.rsvp[1].total;
                     }
                 });
                 eventsService.getRSVPVisitors($scope.event.id).then(
