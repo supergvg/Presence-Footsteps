0bb54e9d6e71f2533010f647cd9201363ca7b3db
Mike Sidorov <mikes.ekb@gmail.com>
2016-09-15 19:20:06 +0500

    GJES-121 Add offline report service.

---
+++ src/app/services/offline-report-service.js
@@ -0,0 +1,62 @@
+'use strict';
+
+(function() {
+  function OfflineReportService ($http, $q) {
+    return {
+      getStats: function (eventId) {
+        // FIXME: temporary solution until server starts sending data
+        return $q.resolve([
+          {
+            guestName: 'Alice',
+            type: 'VIP',
+            list: "Alice's List",
+            reason: 'Guest Not Found',
+            guestExceeded: 0,
+            userName: 'Jocelyn'
+          },
+          {
+            guestName: 'Tim',
+            type: 'Guest',
+            list: "Jocelyn's List",
+            reason: 'Guest List Exceeded',
+            guestExceeded: 3,
+            userName: 'Jocelyn'
+          },
+          {
+            guestName: 'Bill',
+            type: 'Guest',
+            list: "Alice's List",
+            reason: 'Double checked-in',
+            guestExceeded: 0,
+            userName: 'Jocelyn'
+          },
+          {
+            guestName: 'Cathy',
+            type: 'Guest',
+            list: "Alice's List",
+            reason: 'Double checked-in',
+            guestExceeded: 0,
+            userName: 'Jocelyn'
+          },
+          {
+            guestName: 'Tony',
+            type: 'VIP',
+            list: "Jocelyn's List",
+            reason: 'Guest List Exceeded',
+            guestExceeded: 2,
+            userName: 'Jocelyn'
+          }
+        ]);
+        return $http.get('api/OfflineModeController/GetStats/' + eventId);
+      }
+    };
+  }
+
+  OfflineReportService.$inject = [
+    '$http',
+    '$q'
+  ];
+
+  angular.module('gliist').factory('offlineReportService', OfflineReportService);
+})();
+
