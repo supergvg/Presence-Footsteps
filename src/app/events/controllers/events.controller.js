7f7a71f4b7d3597d2c99ae34a0964459960198f8
Mike Sidorov <mikes.ekb@gmail.com>
2016-10-07 20:46:40 +0500

    GJES-1 Add ability to sync FB events.

--- src/app/events/controllers/events.controller.js
+++ src/app/events/controllers/events.controller.js
@@ -1,12 +1,27 @@
 'use strict';
 
-function EventsController ($scope) {
+function EventsController ($scope, facebookService) {
   $scope.$on('facebookEventImport', function (e, event) {
+    $scope.importFacebookEvent(event);
+  });
+
+  $scope.importFacebookEvent = function (event) {
+    $scope.facebookEvent = event;
     $scope.currentEvent = {
       title: event.title
     };
-  });
+  };
+
+  $scope.syncEvent = function () {
+    facebookService.getEventData($scope.facebookEvent.id).then(function (event) {
+      $scope.importFacebookEvent(event);
+    });
+  };
 }
 
+EventsController.$inject = [
+  '$scope',
+  'facebookService'
+];
 
 angular.module('gliist').controller('EventsCtrl', EventsController);
